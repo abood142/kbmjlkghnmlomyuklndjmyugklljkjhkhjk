b6613b58fe510d447a3466ac9615b2b65c20b7da
abood142 <44087788+abood142@users.noreply.github.com>
2018-10-18 19:01:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,6 +43,79 @@ if(mentionned){
 message.channel.sendEmbed(embed);
 }
 });
+    client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'welcome');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return; 
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
+        .addField(':loudspeaker: | نورت السيرفر ي قلبي' , `Welcome to Best | Day, ${member}`)
+        .addField(':id: | user :', "**[" + `${member.id}` + "]**" )
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+               
+                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
+                      
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+                                       
+     .setFooter("**Best | Day**")
+        .setTimestamp()
+    
+      channel.sendEmbed(embed);
+    });
+    client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`
+:heart:  ولكم ولكم نورت يا قلبي
 
+${member} :الإسم
+${member.guild.memberCount} : انت العضو رقم
+
+`) 
+}).catch(console.error)
+})
+client.on('guildMemberAdd', member=> {
+    member.addRole(member.guild.roles.find("name","B|D"));
+    }); 
+      client.on('message', async message => {
+            if(message.content.includes('discord.gg')){ 
+                if(message.member.hasPermission("MANAGE_GUILD")) return;
+        if(!message.channel.guild) return;
+        message.delete()
+          var command = message.content.split(" ")[0];
+    let muterole = message.guild.roles.find(`name`, "Muted");
+    if(!muterole){
+      try{
+        muterole = await message.guild.createRole({
+          name: "Muted",
+          color: "#9c9c9c",
+          permissions:[]
+        })
+        message.guild.channels.forEach(async (channel, id) => {
+          await channel.overwritePermissions(muterole, {
+            SEND_MESSAGES: false,
+            ADD_REACTIONS: false
+          });
+        });
+      }catch(e){
+        console.log(e.stack);
+      }
+    }
+           if(!message.channel.guild) return message.reply('** This command only for servers**');
+     message.member.addRole(muterole);
+    const embed500 = new Discord.RichEmbed()
+      .setTitle("ميوت بسبب نشر")
+            .addField(`**لقد تم إعطائك ميوت كتابي **` , `**السبب: نشر رابط سيرفر في الديسكورد**`)
+            .setColor("c91616")
+            .setThumbnail(`${message.author.avatarURL}`)
+            .setAuthor(message.author.username, message.author.avatarURL)
+        .setFooter(`${message.guild.name} `)
+     message.channel.send(embed500)
+     message.author.send('**`لقد تم إعطاء ميوت بسبب النشر إذا كان عن طريق الخطا فتكلم مع الإدارة`**');
+   
+       
+    }
+})
 
 client.login(process.env.BOT_TOKEN)
