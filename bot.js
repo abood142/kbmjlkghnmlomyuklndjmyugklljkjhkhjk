ee5a4a64e054ccc4c1a7e7833778898692c4676a
abood142 <44087788+abood142@users.noreply.github.com>
2018-10-18 18:41:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,6 +27,22 @@ client.user.setGame(`| %help |`,"https://www.twitch.tv/NICKMERCS")
   console.log('')
   console.log('')
 });
+client.on('message', message => {
+if (message.content.startsWith(prefix + "avatar")) {
+  var mentionned = message.mentions.users.first();
+var x5bzm;
+if(mentionned){
+    var x5bzm = mentionned;
+} else {
+    var x5bzm = message.author;
+    
+}
+  const embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setImage(`${x5bzm.avatarURL}`)
+message.channel.sendEmbed(embed);
+}
+});
 
 
 client.login(process.env.BOT_TOKEN)
