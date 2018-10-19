5fe0b32acb53c2e3ad475ba57a1f8602614f296a
abood142 <44087788+abood142@users.noreply.github.com>
2018-10-19 12:25:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -193,24 +193,7 @@ client.on("message", message => {
    
    }
    });
-client.on('message', message => {
-    if (message.content.startsWith("رابط")) {
 
-  message.channel.createInvite({
-        thing: true,
-        maxUses: 2,
-        maxAge: 86400
-    }).then(invite =>
-      message.author.sendMessage(invite.url)
-    )
-  message.channel.send("``شيك خاصك``")
-
-message.author.send(`**مدة الرابط : يـوم
-عدد استخدامات الرابط : 2**`)
-
-
-    }
-});
    client.on('message', message => {
 	var prefix = "$";
     if (message.content.startsWith(prefix + 'امسح')) {
